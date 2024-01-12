d1bfbac7e327294bb48580f8917fc7dafa5eea8a
Raine Revere <raine@cybersemics.org>
2024-01-12 04:10:13 +0000

    swap test case

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,18 +3,18 @@ import { criticReviews, diff, metameta } from './index.js'
 
 describe('criticReviews', () => {
   test('success', async () => {
-    const result = (await criticReviews('May December'))!
+    const result = (await criticReviews('All of Us Strangers'))!
 
-    expect(result.title).toBe('May December')
-    expect(result.score).toBe(86)
+    expect(result.title).toBe('All of Us Strangers')
+    expect(result.score).toBe(90)
     expect(result.reviews[0]).toEqual({
       author: 'Peter Travers',
-      date: '2023-11-17',
+      date: '2023-12-22',
       publicationName: 'ABC News',
       quote:
-        'What would make a 30-ish woman have sex with a 12-year-old boy? Expect director Todd Haynes to throw you thrillingly off balance with peak acting from Julianne Moore and Charles Melton as the lovers and Natalie Portman as the actress eager to go Hollywood with their squirmy moral tale.',
-      score: 88,
-      url: 'https://abcnews.go.com/GMA/Culture/review-director-todd-haynes-virtuoso-walking-tightrope-humor/story?id=104914907',
+        'Andrew Haigh’s enthralling ghost story concerns a screenwriter (a flawless Andrew Scott) coming to terms with a new love (Paul Mescal) and the parents (Claire Foy and Jamie Bell) who died in his childhood. Watch out for Haigh and his four superlative actors. They’ll get you good.',
+      score: 90,
+      url: 'https://abcnews.go.com/GMA/Culture/review-us-strangers-enthralling-gift-cinematic-hypnosis/story?id=105840158',
     })
   })
 
@@ -25,17 +25,17 @@ describe('criticReviews', () => {
 })
 
 test('diff', async () => {
-  const { reviews } = (await criticReviews('May December'))!
-  const reviewsDiffed = await diff(reviews, 20)
+  const { reviews } = (await criticReviews('All of Us Strangers'))!
+  const reviewsDiffed = await diff(reviews, 95)
   expect(reviewsDiffed[0]).toEqual({
     author: 'Peter Travers',
-    date: '2023-11-17',
-    diff: 68,
+    date: '2023-12-22',
+    diff: -5,
     publicationName: 'ABC News',
     quote:
-      'What would make a 30-ish woman have sex with a 12-year-old boy? Expect director Todd Haynes to throw you thrillingly off balance with peak acting from Julianne Moore and Charles Melton as the lovers and Natalie Portman as the actress eager to go Hollywood with their squirmy moral tale.',
-    score: 88,
-    url: 'https://abcnews.go.com/GMA/Culture/review-director-todd-haynes-virtuoso-walking-tightrope-humor/story?id=104914907',
+      'Andrew Haigh’s enthralling ghost story concerns a screenwriter (a flawless Andrew Scott) coming to terms with a new love (Paul Mescal) and the parents (Claire Foy and Jamie Bell) who died in his childhood. Watch out for Haigh and his four superlative actors. They’ll get you good.',
+    score: 90,
+    url: 'https://abcnews.go.com/GMA/Culture/review-us-strangers-enthralling-gift-cinematic-hypnosis/story?id=105840158',
   })
 })
 
