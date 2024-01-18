9bee2c79418591af58e3648b701e90e256b2d331
Raine Revere <raine@cybersemics.org>
2024-01-18 22:31:39 +0000

    txt-to-csv: Ignore items with no rating.

--- src/txt-to-csv.test.ts
+++ src/txt-to-csv.test.ts
@@ -26,3 +26,32 @@ All of Us Strangers,95`)
 
   await fs.rm(tempDir, { force: true, recursive: true })
 })
+
+test('ignore items with no rating', async () => {
+  const tempDir = await fs.mkdtemp(path.join(os.tmpdir(), 'metameta-'))
+  const sampleFile = 'sample.txt'
+  const inputFile = path.join(tempDir, sampleFile)
+  fs.writeFile(
+    inputFile,
+    `- Movies
+  - =sort
+    - Alphabetical
+  - May December
+    - 20
+      - The soap opera styling and cringe-worthy score oversucceeded
+  - All of Us Strangers
+    - 95
+      - Heartbreakingly beautiful`,
+  )
+
+  await spawn('node', ['build/txt-to-csv.js', inputFile])
+
+  const outputFile = path.join(tempDir, 'sample.csv')
+  const output = await fs.readFile(outputFile, 'utf8')
+
+  expect(output).toBe(`title,rating
+May December,20
+All of Us Strangers,95`)
+
+  await fs.rm(tempDir, { force: true, recursive: true })
+})
--- src/txt-to-csv.ts
+++ src/txt-to-csv.ts
@@ -4,6 +4,9 @@ import path from 'path'
 import { parse } from 'text-block-parser'
 import { json2csv } from 'json-2-csv'
 
+/** Returns true if a value is non null and non undefined. */
+const nonNull = <T>(value: T | null | undefined): value is T => value != null
+
 const [file] = process.argv.slice(2)
 
 if (!file) {
@@ -14,12 +17,14 @@ if (!file) {
 const text = await fs.readFile(file, 'utf8')
 const root = parse(text)
 const movies = root[0].children
-const ratings = movies.map(block => {
-  const title = block.scope.replace(/\s*- /, '')
-  const ratingBlock = block.children.find(child => /- \d+/.test(child.scope))
-  const rating = parseInt(ratingBlock?.scope.replace(/\s*- /, '')!, 10)
-  return { title, rating }
-})
+const ratings = movies
+  .map(block => {
+    const title = block.scope.replace(/\s*- /, '')
+    const ratingBlock = block.children.find(child => /- \d+/.test(child.scope))
+    const rating = ratingBlock?.scope.replace(/\s*- /, '')
+    return rating ? { title, rating } : null
+  })
+  .filter(nonNull)
 
 const csv = json2csv(ratings)
 const ext = path.extname(file)
